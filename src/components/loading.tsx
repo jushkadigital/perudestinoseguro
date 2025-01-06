de45a2a58ec5451363edfa800d115de627a08c12
josh <urgosxd@gmail.com>
2025-01-06 12:37:30 -0500

    aoe

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,17 +1,18 @@
 'use client'
 import React, { CSSProperties } from "react"
-import ClipLoader from 'react-spinners/ClipLoader';
 
 interface Props {
   sty?:string
 }
 export default function Loader({sty=""}:Props){
-  const override: CSSProperties = {
-  display: "block",
-  margin: "0 auto",
+  // const override: CSSProperties = {
+  // display: "block",
+  // margin: "0 auto",
   // borderColor: "red",
-};
-  return (<div className={`mt-28 ${sty}`}>
-      <ClipLoader  cssOverride={override} loading={true} color="#D20000" size={100}/>
-    </div>)
-}
+  return (
+  <div></div>
+  // <div className={`mt-28 ${sty}`}>
+  //     <ClipLoader  cssOverride={override} loading={true} color="#D20000" size={100}/>
+  //   </div>
+    )
+    }
